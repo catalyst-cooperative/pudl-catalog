4031b8a55a86ac023f784e828435096e9a88f497
Zane Selvans <zane.selvans@catalyst.coop>
2022-04-19 20:28:55 -0500

    Remove obsolete TestEpaCemsParquet class
    Removed this class / module because most of its functionality has been
    replicated in the tests, and the performance exploration it facilitated
    isn't needed any more.
    
    Also added some additional package metadata to the top level `__init__.py`

--- src/pudl_catalog/__init__.py
+++ src/pudl_catalog/__init__.py
@@ -1,9 +1,11 @@
 """An installable catalog of open data related to the US energy system."""
+import logging
 import os
 import warnings
 from pathlib import Path
 
 import intake
+import pkg_resources
 
 import pudl_catalog.helpers  # noqa: F401
 
@@ -34,3 +36,16 @@ if os.getenv("PUDL_INTAKE_CACHE") is None:
 _pudl_catalog_path = Path(__file__).parent.resolve() / "pudl_catalog.yaml"
 # After installation the catalog intake.cat.pudl_cat should be available
 pudl_cat = intake.open_catalog(_pudl_catalog_path)
+
+__author__ = "Catalyst Cooperative"
+__contact__ = "pudl@catalyst.coop"
+__license__ = "MIT License"
+__maintainer__ = "Zane Selvans"
+__maintainer_email__ = "zane.selvans@catalyst.coop"
+__version__ = pkg_resources.get_distribution("catalystcoop.pudl_catalog").version
+__docformat__ = "restructuredtext en"
+__description__ = "An catalog of open data related to the US energy system."
+__projecturl__ = "https://github.com/catalyst-cooperative/pudl-catalog"
+__downloadurl__ = "https://github.com/catalyst-cooperative/pudl-catalog"
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
