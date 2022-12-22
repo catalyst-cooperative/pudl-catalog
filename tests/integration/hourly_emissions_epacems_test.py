3fb7cad333be12a378b8ef9ec04e58771030c578
bendnorman <bdn29@cornell.edu>
2022-12-22 12:25:05 -0800

    Skip gcs integration tests to avoid egress fees

--- tests/integration/hourly_emissions_epacems_test.py
+++ tests/integration/hourly_emissions_epacems_test.py
@@ -82,6 +82,11 @@ def test_read_parquet(
     expected_df: pd.DataFrame,
 ) -> None:
     """Test direct access via read_parquet()."""
+    if protocol == "gs":
+        pytest.skip(
+            "Skip GCS test until we figure out how to pull smaller partitions of the data to avoid large egress fees."
+        )
+
     logger.debug(f"read_parquet, {protocol=}, {partition_suffix=}")
     epacems_url = parquet_url(
         protocol=protocol,
@@ -119,6 +124,11 @@ def test_intake_catalog(
     tmp_path: Path,
 ) -> None:
     """Test reading data from the intake catalog."""
+    if protocol == "gs":
+        pytest.skip(
+            "Skip GCS test until we figure out how to pull smaller partitions of the data to avoid large egress fees."
+        )
+
     logger.debug(f"intake_catalog, {protocol=}, {partition_suffix=}")
     os.environ["PUDL_INTAKE_PATH"] = BASE_URLS[protocol]
     # Save the data to a temporary directory
